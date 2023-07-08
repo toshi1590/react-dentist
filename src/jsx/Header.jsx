e4b33ecbf727706dd3d67cb08418c16aeb75386b
toshiki murai <1590a1590a@gmail.com>
2023-07-08 15:55:14 +0900

    created reservatopm function

--- src/jsx/Header.jsx
+++ src/jsx/Header.jsx
@@ -16,7 +16,6 @@ export const Header = () => {
         <NavLink className={HeaderModule.tab} style={({ isActive }) => (isActive ? { color: 'blue' } : undefined)} to="/patient_register">Patient Register</NavLink>
         <NavLink className={HeaderModule.tab} style={({ isActive }) => (isActive ? { color: 'blue' } : undefined)} to="/reservation">Reservation</NavLink>
         <NavLink className={HeaderModule.tab} style={({ isActive }) => (isActive ? { color: 'blue' } : undefined)} to="/mypage">My page</NavLink>
-        {/* <NavLink className={HeaderModule.tab} style={({ isActive }) => (isActive ? { color: 'blue' } : undefined)} to="/signin">Sign in</NavLink> */}
       </div>
     </header>
   );
--- src/jsx/Reservation.jsx
+++ src/jsx/Reservation.jsx
@@ -1,20 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const Reservation = (props) => {
-  const [text, setText] = useState()
+  const [reservations, setReservations] = useState([]);
 
-  fetch(`https://dummyjson.com/products/${props.day}`)
-  .then((res) => {
-    return res.json(); 
-  })
-  .then((json) => {
-    setText(json.description);
-  })
+  const tds = reservations.map(reservation => {
+    return (
+      <>
+        <div>
+          <span>date: {reservation.date}</span>
+          <span>---</span>
+          <span>patient id: {reservation.patient_id}</span>
+        </div>    
+      </>
+    );
+  });
+
+  useEffect(() => {
+    fetch(`http://localhost:8000/api/reservations/${props.year}/${props.month}/${props.day}`)
+    .then(res => res.json())
+    .then(json_data => {
+      setReservations([]);
+      json_data.map(reservation => {
+        setReservations(reservations => [...reservations, {date: reservation.date, patient_id: reservation.patient_id}]);
+      });
+    })
+  }, [props])
  
   return (
     <>
-      <div>{props.year} {props.month} {props.day}</div>
-      <div>{text}</div>
+      <div>{tds}</div>
     </>
   );
 }
